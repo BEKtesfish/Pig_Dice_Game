7f72daff750ba7e5cc1cdd1de072bd1ecf79bbf2
bereket tesfaye <berekettesfaye7909@gmail.com>
2024-03-08 05:50:37 +0100

    created palyer hold  method for the game  class

--- src/pig_dice/game.py
+++ src/pig_dice/game.py
@@ -324,4 +324,20 @@ Holds: {player1.get_num_holds():<15}Holds: {player2.get_num_holds()}
         """
         choice=input("Hold or roll ('h', 'r','exit'): ")
         return choice
+    
+    def player_hold(self,current_player,num):
+        """
+    Handle the player holding the dice.
+
+    Parameters:
+    - current_player (Player): The current player.
+    - num (int): The number rolled on the dice.
+
+    Returns:
+    str: A message indicating that the player holds and their overall score.
+    """
+        current_player.set_num_holds()
+        self.current_score+=num
+        current_player.set_score(self.current_score)
+        return f"'{current_player.get_name()}' holds, Overall score: {current_player.get_score()}\n"
             
