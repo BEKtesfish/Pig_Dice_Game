70494865178f10471e7d3605b3ac77ae14f991c3
bereket tesfaye <berekettesfaye7909@gmail.com>
2024-03-08 05:39:59 +0100

    created a play method for my class

--- src/pig_dice/game.py
+++ src/pig_dice/game.py
@@ -33,4 +33,71 @@ class Game:
         self.game_played=0
         self.winner=''
         self.player_list=[]
+        
+    def play(self):
+        """Start the game and handle player interactions."""
+        while True:
+            try:
+                print(self.display())
+                choice=self.take_choice_menu()
+                if choice == '1':
+                    player1,player2=self.set_player_multi()
+                    self.player1=player1
+                    self.player2=player2
+                    self.player_list=[self.player1,self.player2]
+                    while True:
+                        try:
+                            print(self.display_multiplay())
+                            choice2=self.take_choice_menu()
+                            if choice2== '1':
+                                self.multiplay()
+                            elif choice2 == '2':
+                                self.multiplay_change_name()
+                            elif choice2 == '3':
+                                if self.game_played != 0:
+                                    Save().save(self.player1,self.player2,self.game_played)
+                                    self.game_played=0
+                                    
+                                break
+                            else:
+                                raise ValueError("\nInvalid input! Enter a number from the listed options.\n ")
+                        except ValueError as e:
+                            print(e)
+
+                elif choice == '2':
+                
+                    self.player1=self.set_player_single()
+                    self.computer=Computer()
+                    self.player_list=[self.player1,self.computer]
+
+                    while True:
+                        try:
+                            print(self.single_game_menu())
+                            choice2=self.take_choice_menu()
+                            if choice2 == '1':
+                                self.single_game_intro("Easy")
+                                self.single_game(Computer().easy_mode)
+                            elif choice2 == '2':
+                                self.single_game_intro("Medium")
+                                self.single_game(Computer().medium_mode)
+                            elif choice2 == '3':
+                                self.single_game_intro("Hard")
+                                self.single_game(Computer().hard_mode)
+                            elif choice2 == '4':
+                                self.singleplay_change_name()
+                            elif choice2 == '5':
+                                if self.game_played != 0:
+                                    Save().save(self.player1,self.computer,self.game_played)
+                                    self.game_played=0
+                                break
+                            else:
+                                raise ValueError("\nInvalid input! Enter a number from the listed options.\n ")
+                        except ValueError as e:
+                            print(e)    
+                elif choice == "3":
+                    break
+                else:
+                    raise ValueError("\nInvalid input! Enter a number from the listed options.\n")
+            except ValueError as e:
+                            print(e)
     
