c0606d08b935449f8267e2e218fef2b3973d789f
bereket tesfaye <berekettesfaye7909@gmail.com>
2024-03-08 05:12:03 +0100

    created a get name and set name for the palyer class

---
+++ src/pig_dice/display.py
@@ -0,0 +1,55 @@
+
+
+def rules():
+    
+    return """
+Pig Dice Game Rules:
+
+Objective:
+The objective of Pig Dice is to be the first player to reach or exceed a total score of 100 points.
+
+Equipment:
+- One six-sided dice
+- A scorecard or a way to keep track of points
+
+Gameplay:
+1. Starting the Game:
+   - The game begins with two players, taking turns to roll the dice.
+   - Players can choose to play in single-player mode against the computer or in multiplayer mode against another
+     player.
+   - Note: Type 'start' and follow the instruction.
+
+2. Taking Turns:
+   - On a player's turn, they roll the dice.
+   - If the player rolls:
+     - roll 1: They lose all points accumulated during that turn, and it becomes the next player's turn.
+     - Any other number: The number rolled is added to the player's turn total, and they can choose to hold or 
+     roll again.
+
+3. Holding:
+   - If a player chooses to hold, the turn total is added to their overall score, and it becomes the next player's
+     turn.
+   - Holding allows the player to secure their points for that turn and add them to their total score.
+
+4. Winning the Game:
+   - The game continues until one player reaches or exceeds a total score of 100 points.
+   - Once a player reaches the winning score, they are declared the winner, and the game ends.
+
+5. Cheat Mode:
+   - Optionally, players can enable cheat mode where the dice always roll a six. This can be used for testing or
+     fun purposes but should be disabled during regular gameplay.
+   - Note: Type 'cheat' and press Enter to activate cheat mode and start the game.
+
+6. High Score:
+   - Players have the option to see the high score.
+   - Note: Type 'high_score' or 'score'.
+
+7. Difficulty Levels:
+   - Players can choose from different difficulty levels when playing against the computer, such as Easy, Medium, 
+   or Hard. Each difficulty level may alter the computer's strategy or behavior.
+
+8. Exit:
+   - While playing, type 'exit' to terminate and restart the game.
+
+These are the basic rules of the Pig Dice game. Enjoy playing!
+"""
